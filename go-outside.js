2ae0a19b83b1c1e980e02c26874adb4cf4dd8fa4
donald <donald.ma@hotmail.ca>
2017-05-05 21:29:24 +0000

    Adds conditionals excercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var temperature = function(prompt("What is the temperature today?"));
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
